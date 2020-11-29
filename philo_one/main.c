ecca47637966d76e056fe64bbd3f2620e0da3d39
merel mourik <merelmourik@student.42.fr>
2020-11-29 14:26:22 +0100

    initialize philo without segfault

--- philo_one/main.c
+++ philo_one/main.c
@@ -6,38 +6,25 @@
 /*   By: merelmourik <merelmourik@student.42.fr>      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/25 12:27:55 by merelmourik   #+#    #+#                 */
-/*   Updated: 2020/11/29 13:31:22 by merelmourik   ########   odam.nl         */
+/*   Updated: 2020/11/29 14:25:54 by merelmourik   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
 
-// void	*initialize_philo(void *data_ptr)
-// {
-// 	t_philo	*philo;
-// 	int		i;
-
-// 	philo = data_ptr;
-// 	i = 1;
-// 	// long time = time_stamp();
-// 	while (i <= philo->data->philo_amount)
-		
-// 	return (NULL);
-// }
-
-t_philo	**initialize_philosophers(t_data *data)
+t_philo	*initialize_philosophers(t_data *data)
 {
-	t_philo	**philo;
+	t_philo	*philo;
 	int		i;
 
 	philo = malloc(sizeof(t_philo) * data->philo_amount);
 	i = 0;
 	while (i < data->philo_amount)
 	{
-		(*philo)[i].id = i + 1;
-		(*philo)[i].data = data;
-		(*philo)[i].amount_eaten = 0;
-		(*philo)[i].alive = 0;
+		philo[i].id = i + 1;
+		philo[i].data = data;
+		philo[i].amount_eaten = 0;
+		philo[i].alive = 0;
 		i++;
 	}
 	return (philo);
@@ -46,7 +33,7 @@ t_philo	**initialize_philosophers(t_data *data)
 int	main(int argc, char **argv)
 {
 	t_data		*data;
-	t_philo		**philo;
+	t_philo		*philo;
 	pthread_t	*thread;
 
 	if (!(data = malloc(sizeof(t_data))))
@@ -59,7 +46,11 @@ int	main(int argc, char **argv)
 	thread = malloc(sizeof(pthread_t) * data->philo_amount);
 	data->fork = malloc(sizeof(pthread_mutex_t) * data->philo_amount);
 	philo = initialize_philosophers(data);
-	// printf("%d\n", data->eat);
+	// int i = 0;
+	// while (i < data->philo_amount){
+	// 	printf("id philo = %d\n", philo[i].id);
+	// 	i++;
+	// }
 	system("leaks philo_one");
 	return (0);
 }
