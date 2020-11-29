06cbae9125680fe47106e31dee82292de99389d6
merel mourik <merelmourik@student.42.fr>
2020-11-29 17:08:21 +0100

    small progress, no leaks and nog segfaults

--- philo_one/main.c
+++ philo_one/main.c
@@ -6,7 +6,7 @@
 /*   By: merelmourik <merelmourik@student.42.fr>      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/25 12:27:55 by merelmourik   #+#    #+#                 */
-/*   Updated: 2020/11/29 14:25:54 by merelmourik   ########   odam.nl         */
+/*   Updated: 2020/11/29 17:07:58 by merelmourik   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,44 @@ t_philo	*initialize_philosophers(t_data *data)
 	{
 		philo[i].id = i + 1;
 		philo[i].data = data;
-		philo[i].amount_eaten = 0;
-		philo[i].alive = 0;
+		philo[i].repetition = 0;
+		philo[i].left = i;
+		// nog even checken of deze gedachte gang klopt
+		philo[i].right = (i == data->philo_amount - 1 ? 0 : i + 1);
+		philo[i].status = ALIVE;
 		i++;
 	}
 	return (philo);
 }
 
-int	main(int argc, char **argv)
+void	*activate_philo(void *philosopher)
+{
+	t_philo *philo;
+
+	philo = philosopher;
+	return NULL;
+}
+
+void	philosopher_threads(t_philo *philo)
+{
+	pthread_t	*thread;
+	int			i;
+
+	thread = malloc(sizeof(pthread_t) * philo->data->philo_amount);
+	i = 0;
+	while (i < philo->data->philo_amount)
+	{
+		pthread_create(&thread[i], NULL, activate_philo, &philo[i]);
+		pthread_join(thread[i], NULL);
+		i++;
+	}
+	free (thread);
+}
+
+int		main(int argc, char **argv)
 {
 	t_data		*data;
 	t_philo		*philo;
-	pthread_t	*thread;
 
 	if (!(data = malloc(sizeof(t_data))))
 		return (-1);
@@ -43,14 +69,19 @@ int	main(int argc, char **argv)
 		free(data);
 		return (-1);
 	}
-	thread = malloc(sizeof(pthread_t) * data->philo_amount);
 	data->fork = malloc(sizeof(pthread_mutex_t) * data->philo_amount);
 	philo = initialize_philosophers(data);
+	// free(data);
+	philosopher_threads(philo);
+	// ik heb genoeg threads and ik heb aan de filosofen de juiste data gegeven
+	// maar nu moet elke filosoof ook nog aan thread worden gekoppeld
 	// int i = 0;
 	// while (i < data->philo_amount){
 	// 	printf("id philo = %d\n", philo[i].id);
 	// 	i++;
 	// }
+	free(philo);
+	free(data->fork);
 	system("leaks philo_one");
 	return (0);
 }
--- philo_one/philo_one.h
+++ philo_one/philo_one.h
@@ -6,7 +6,7 @@
 /*   By: merelmourik <merelmourik@student.42.fr>      +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/25 12:30:17 by merelmourik   #+#    #+#                 */
-/*   Updated: 2020/11/29 13:30:29 by merelmourik   ########   odam.nl         */
+/*   Updated: 2020/11/29 16:55:27 by merelmourik   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,11 @@
 # include <pthread.h>
 # include <string.h>
 
+typedef enum		e_status {
+	ALIVE,
+	DEAD
+}					t_status;
+
 typedef struct		s_data {
 	int				philo_amount;
 	int				die;
@@ -32,12 +37,14 @@ typedef struct		s_data {
 typedef struct		s_philo
 {
 	int				id;
-	int				amount_eaten;
 	int				time_until_death;
 	int				start;
-	int				alive;
+	int				repetition;
+	int				left;
+	int				right;
 	pthread_mutex_t	talking;
 	t_data			*data;
+	t_status		status;
 }					t_philo;
 
 int		ft_atoi(const char *str);
